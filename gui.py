cb395f1ad9524227c6c3c1a50f42446d0a3e7a6e
Jason Castaño <jasonale3007@gmail.com>
2022-04-14 23:58:56 -0500

    date label added

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from datetime import datetime
+from datetime import date
 import math
 class Window(Frame):
 
@@ -13,20 +14,25 @@ class Window(Frame):
         self.master.title("Desfibrilador")
         self.pack(fill=BOTH,expand=True)
         self.configure(bg="#353231")
-        self.time_label = Label(self,text="10:40:30",bg="#353231",fg="#EEE110")
-        #self.time_label.place(x=50,y=150)
-        #self.time_label.place(x=10,y=10)
+        self.time_label = Label(self,bg="#353231",fg="#EEE110")
         self.time_label.place(x=math.floor(width*0.55), y=math.floor(height*0.01))
         self.time_now()
+        self.date_label = Label(self,bg="#353231",fg="#EEE110")
+        self.date_label.place(x=math.floor(width*0.7), y=math.floor(height*0.01))
+        self.date_now()
     def time_now(self):
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
         self.time_label.config(text=current_time)
         self.time_label.after(1000,self.time_now)
+    def date_now(self):
+        now = date.today()
+        current_date = now.strftime("%b-%d-%Y")
+        self.date_label.config(text=current_date)
+        self.date_label.after(3600000,self.date_now)
 root = Tk()
 width = root.winfo_screenwidth()
 height = root.winfo_screenheight()
 root.geometry(f'{width}x{height}')
-#root.configure(bg="#353231")
 app = Window(root)
 root.mainloop()
