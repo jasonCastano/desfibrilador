a24579d848b19ec58d1d24df43c048ab38b794a9
Jason Castaño <jasonale3007@gmail.com>
2022-05-31 18:55:09 -0500

    launch desfibrilador

--- launch_desfibrilador.sh
+++ launch_desfibrilador.sh
@@ -1,8 +1,7 @@
 sudo chmod 777 /dev/ttyACM0
 sudo chmod 777 /dev/ttyACM1
-roscore &
-rosrun desfibrilador gui.py &
 rosrun desfibrilador ecg_signal.py &
-rosrun desfibrilador comandos_modos.py &
 rosrun desfibrilador lpm.py &
+rosrun desfibrilador comandos_modos.py &
+rosrun desfibrilador gui.py &
 wait
