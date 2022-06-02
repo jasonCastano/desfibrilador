58cdda42273c507b12012dec7f07225c27fd1a49
Jason Castaño <jasonale3007@gmail.com>
2022-06-01 22:55:52 -0500

    prueba solo ecg

--- launch_desfibrilador.sh
+++ launch_desfibrilador.sh
@@ -1,7 +1,7 @@
 sudo chmod 777 /dev/ttyACM0
-sudo chmod 777 /dev/ttyACM1
+#sudo chmod 777 /dev/ttyACM1
 rosrun desfibrilador ecg_signal.py &
 rosrun desfibrilador lpm.py &
-rosrun desfibrilador comandos_modos.py &
+#rosrun desfibrilador comandos_modos.py &
 rosrun desfibrilador gui.py &
 wait
